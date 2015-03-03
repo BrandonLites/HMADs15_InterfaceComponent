34ccc470e05ccb4ab16a8e748965af8630b8d154
blites <blites@unm.edu>
2015-03-03 03:12:16 -0700

    Initial commit

---
+++ app/src/main/java/edu/unm/cs/blites/swipesearchspike/TestActivity.java
@@ -0,0 +1,100 @@
+package edu.unm.cs.blites.swipesearchspike;
+
+import android.content.Intent;
+import android.graphics.Point;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+
+public class TestActivity extends ActionBarActivity implements GestureDetector.OnGestureListener {
+    private GestureDetector gDetector;
+    float screenWidth;
+    float screenHeight;
+    float touchThreshold;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        gDetector = new GestureDetector(this, this);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = (float) size.x;
+        screenHeight = (float)size.y;
+
+        touchThreshold = screenHeight - (screenHeight / 50);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        return gDetector.onTouchEvent(e);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float y1 = e1.getY();
+        float y2 = e2.getY();
+
+        if(y1 > y2 && y1 > touchThreshold) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+}
